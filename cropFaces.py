adbc6c49c20f3052b07172b74e7123bec05aea90
mlubega <3893519+mlubega@users.noreply.github.com>
2019-04-14 12:51:21 +0100

    added logging to console/file

--- cropFaces.py
+++ cropFaces.py
@@ -16,17 +16,34 @@ import sys
 import os
 import random 
 from sklearn.model_selection import train_test_split
+import logging
 
 
-RAND_COUNT = 10
-VID_SAMPLE = 3
-NUM_CLASSES = 3
+logger = logging.getLogger('simple_example')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('cropFaces.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(message)s')
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+# add the handlers to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
+
+RAND_COUNT = 30
+VID_SAMPLE = 10
+NUM_CLASSES = 0
 IMG_EXTS = ['.jpg', '.jpeg']
 VID_EXTS = ['.mp4', '.mov']
 
 face_detector = cv2.CascadeClassifier('/home/aries/opencv-4.0.1/data/haarcascades/haarcascade_frontalface_default.xml')
 src_path = '/home/aries/ds_course/term2/compv/coursework/test/'
-dst_path = '/home/aries/ds_course/term2/compv/coursework/cropped/'
+dst_path = '/home/aries/ds_course/term2/compv/coursework/tcrop/'
 
 
 def detectFaceAndCrop(img):
@@ -34,16 +51,13 @@ def detectFaceAndCrop(img):
     cropped_faces = []
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    if (len(faces) > 1):
+        logger.debug("%i bboxes found in image", len(faces))
     for (x,y,w,h) in faces:     
         #(x,y,w,h) = increaseBBOX(face_box, 10) 
         crop = img[y:y+h, x:x+w]
         cropped_faces.append(crop)
-
-#        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)   
-#        cv2.imshow('img', img)
-#        if cv2.waitKey(0) & 0xFF == 27:
-#            break
-    
+        
     return cropped_faces
 
 def writeCrops(crop_list, dest):
@@ -52,24 +66,23 @@ def writeCrops(crop_list, dest):
             
     for i in range(0, len(crop_list)):
         filename = str(i) + ".jpg"
-        cv2.imwrite(os.path.join(dest, filename), crop_list[i])
-        
+        cv2.imwrite(os.path.join(dest, filename), crop_list[i])      
     return
         
 
 def generateCleanData():
         
     people = os.listdir(src_path) 
-    
+    NUM_CLASSES = len(people)
     
     for person in people:
         
-        print("Processing Sub Dir", person)
+        logger.debug("Processing Person %s ----------",  person)
         full_img_dir = os.path.join(src_path, person)
         jpgs = list(filter(lambda x: x.endswith(tuple(IMG_EXTS)), os.listdir(full_img_dir)))
         vids = list(filter(lambda x: x.endswith(tuple(VID_EXTS)), os.listdir(full_img_dir)))
     
-        ## Save cropped images
+        ## holds cropped images from both img & vids
         crop_shots = []
     
             
@@ -77,7 +90,7 @@ def generateCleanData():
         for pic in jpgs:
             img = cv2.imread(os.path.join(src_path, person, pic))
             crop_shots += detectFaceAndCrop(img)     
-        print(len(crop_shots), "cropped images from from images")
+        logger.debug("%i cropped images from from images", len(crop_shots))
         
         #%% read and crop video files
         for vid in vids:
@@ -88,8 +101,10 @@ def generateCleanData():
                 sys.exit('Cap Not Open')
                 
             total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-            frames_to_sample = random.sample(range(1, total_frames), RAND_COUNT)
-            print("Sampling", frames_to_sample,"of", total_frames, "frames")
+            sample_size = min(RAND_COUNT, total_frames)
+            
+            frames_to_sample = random.sample(range(1, total_frames), sample_size)
+            #print("Sampling", sample_size,"of", total_frames, "total frames")
             
             
             crops_taken = 0
@@ -106,24 +121,26 @@ def generateCleanData():
                     
                 shots = detectFaceAndCrop(frame)
                 if not shots:
-                    print("Crop Not Taken, loop  until a crop is found")
+                    #print("Crop Not Taken, loop  until a crop is found")
                     continue;
-                print(len(shots),"bboxs found in image" )
                 crop_shots += shots
                 crops_taken += len(shots)
                 if crops_taken >= VID_SAMPLE:
                     break;
                 
-            print(crops_taken, "cropped images found from video", vid)
+            logger.debug("%i cropped images found from video %s", crops_taken, vid)
             cap.release
+        
+        logger.debug("%i found for person %s \n\n",  len(crop_shots),  person)
             
             
-            #%% make train/test 
-            train, test = train_test_split(crop_shots, train_size=0.8, random_state=45, shuffle=True)
             
-            #%% Write cropped images
-            writeCrops(train, os.path.join(dst_path, "train", person))
-            writeCrops(test, os.path.join(dst_path, "test", person))
+        #%% make train/test 
+        train, test = train_test_split(crop_shots, train_size=0.8, random_state=45, shuffle=True)
+        
+        #%% Write cropped images
+        writeCrops(train, os.path.join(dst_path, "train", person))
+        writeCrops(test, os.path.join(dst_path, "test", person))
       
 
 
