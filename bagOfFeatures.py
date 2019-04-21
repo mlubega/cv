3d5a1d72e3116356ea8bb65bd0bec9f2448ff973
mlubega <3893519+mlubega@users.noreply.github.com>
2019-04-21 21:52:13 +0100

    fine-tuning ocr process

--- bagOfFeatures.py
+++ bagOfFeatures.py
@@ -25,26 +25,31 @@ TEST_DATA  = '/home/aries/ds_course/term2/compv/coursework/test_crops/test/'
 
 NUM_CLASSES = len(os.listdir(TRAIN_DATA))
 K = NUM_CLASSES * 5  # KMeans Classes
+sift = cv2.xfeatures2d.SIFT_create()
+surf = cv2.xfeatures2d.SURF_create()
+# Initiate FAST detector
+star = cv2.xfeatures2d.StarDetector_create()
+# Initiate BRIEF extractor
+brief = cv2.xfeatures2d.BriefDescriptorExtractor_create()
+# Initiate ORB detector
+orb = cv2.ORB_create()
 
 #%% Helper Functions
 
 def getSIFTfeatures(gray_img):
-    sift = cv2.xfeatures2d.SIFT_create() #What threshold?
+     #What threshold?
     kp, desc = sift.detectAndCompute(gray_img, None)
     return kp, desc
 
 def getSURFfeatures(gray_img):  
-    surf = cv2.xfeatures2d.SURF_create() #what threshold?
+     #what threshold?
     surf.setExtended(True) # --> to expand to 128 dim.
     kp, desc = surf.detectAndCompute(gray_img, None)
     return kp, desc
 
 
 def getBRIEFfeatures(gray_img):
-    # Initiate FAST detector
-    star = cv2.xfeatures2d.StarDetector_create()
-    # Initiate BRIEF extractor
-    brief = cv2.xfeatures2d.BriefDescriptorExtractor_create()
+
     # find the keypoints with STAR
     kp = star.detect(gray_img, None)
     # compute the descriptors with BRIEF
@@ -52,8 +57,7 @@ def getBRIEFfeatures(gray_img):
     return kp, desc
 
 def getORBfeatures(gray_img):
-    # Initiate ORB detector
-    orb = cv2.ORB_create()
+
     # find the keypoints with ORB
     kp = orb.detect(gray_img,None)
     # compute the descriptors with ORB
@@ -72,21 +76,24 @@ def extractFeatures(crop_list, featureFunc):
     return descriptors
 
 
+def groupAllFeatures(desc_hash):
+    all_desc = []
+    for person, crops in desc_hash.items():
+        for idx, descs in crops.items():
+            all_desc.extend(descs)
+            
+    return all_desc
+        
+
 def trainKMeans(descriptors):
      
-    des = list(descriptors.values())
-    desc_matrix = reduce(lambda x,y: np.concatenate((x,y)), des)
-
     # Train KMeans
     initial_size = 3 * K
-    batch_size = len(descriptors.keys()) * 3  # What's a good metric to determine this number? 
-    kmeans = MiniBatchKMeans(n_clusters=K, batch_size=batch_size, init_size=initial_size, verbose=0).fit(desc_matrix)
+    batch_size = int(len(descriptors) / 3)  # What's a good metric to determine this number? 
+    kmeans = MiniBatchKMeans(n_clusters=K, batch_size=batch_size, init_size=initial_size, verbose=0).fit(descriptors)
     
     return kmeans
     
-    
-
-
 def generateHistograms(descriptors, kmeans_centers):
     
     ## Generate Histogram 
@@ -103,52 +110,90 @@ def generateHistograms(descriptors, kmeans_centers):
 
 sift_train_feature_vec = [[], []]   
 sift_test_feature_vec = [[], []]  
-sift_kmeans = {} 
+sift_kmeans = None
 
 surf_train_feature_vec = [[], []]   
 surf_test_feature_vec = [[], []]  
-surf_kmeans = {} 
+surf_kmeans = None
 
 orb_train_feature_vec = [[], []]   
 orb_test_feature_vec = [[], []]   
-orb_kmeans = {}
+orb_kmeans = None
 
 #%% Process each folder    
 
 print("Training Data")
 people = os.listdir(TRAIN_DATA)    
-    
+
+
+allSIFT = {}
+allSURF = {}
+allORB = {}
+
+#%% Extract Features
+
 for person in people:
     
     print("Processing Sub Dir", person)
     
-    #TRAIN DATA
+    
     
     #read in cropped data
     crop_names = os.listdir(os.path.join(TRAIN_DATA, person))
     crop_names = list(map(lambda x: os.path.join(TRAIN_DATA, person, x), crop_names)) 
     crops = [cv2.imread(x , cv2.IMREAD_GRAYSCALE) for x in crop_names ]
     
-        
     # get SIFT Features
     sift_desc = extractFeatures(crops, getSIFTfeatures)
-    sift_kmeans[person] = trainKMeans(sift_desc)
-    sift_histograms = generateHistograms(sift_desc, sift_kmeans[person])
+    allSIFT[person] = sift_desc
+    print("Extracted SIFT")
+    
+    # get SURF Features
+    surf_desc = extractFeatures(crops, getSURFfeatures)
+    allSURF[person] = surf_desc
+    print("Extracted SURF")
+    
+    # get ORB Features
+    orb_desc = extractFeatures(crops, getORBfeatures)
+    allORB[person] = orb_desc
+    print("Extracted ORB")
+
+#%% Group Features
+
+sift_matrix = groupAllFeatures(allSIFT)
+surf_matrix = groupAllFeatures(allSURF)
+orb_matrix = groupAllFeatures(allORB)
+
+#%% Train KMeans
+
+print("Training SIFT KMeans")
+sift_kmeans = trainKMeans(sift_matrix)
+
+print("Training SURF KMeans")
+surf_kmeans = trainKMeans(surf_matrix)
+
+print("Training ORB KMeans")
+orb_kmeans = trainKMeans(orb_matrix)
+ 
+#%% Create Train Histograms    
+    
+for person in people:
+    
+    print("Creating Train Histograms for", person)
+    
+    sift_histograms = generateHistograms(allSIFT[person], sift_kmeans)
     sift_train_feature_vec[0].extend(sift_histograms)
     sift_train_feature_vec[1].extend([person] * len(sift_histograms))
     print("Extracted SIFT")
+    
     # get SURF Features
-    surf_desc = extractFeatures(crops, getSURFfeatures)
-    surf_kmeans[person] = trainKMeans(surf_desc)
-    surf_histograms = generateHistograms(surf_desc, surf_kmeans[person])
+    surf_histograms = generateHistograms(allSURF[person], surf_kmeans)
     surf_train_feature_vec[0].extend(surf_histograms)
     surf_train_feature_vec[1].extend([person] * len(surf_histograms))
     print("Extracted SURF")
     
     # get ORB Features
-    orb_desc = extractFeatures(crops, getORBfeatures)
-    orb_kmeans[person] = trainKMeans(orb_desc)
-    orb_histograms = generateHistograms(orb_desc, orb_kmeans[person])
+    orb_histograms = generateHistograms(allORB[person], orb_kmeans)
     orb_train_feature_vec[0].extend(orb_histograms)
     orb_train_feature_vec[1].extend([person] * len(orb_histograms))
     print("Extracted ORB")
@@ -161,38 +206,30 @@ people = os.listdir(TEST_DATA)
 for person in people:
     #TEST DATA
     
-    print("Processing Sub Dir", person)
+    print("Creating Test Histograms for", person)
     
-    #read in cropped data
-    crop_names = os.listdir(os.path.join(TEST_DATA, person))
-    crop_names = list(map(lambda x: os.path.join(TEST_DATA, person, x), crop_names)) 
-    crops = [cv2.imread(x , cv2.IMREAD_GRAYSCALE) for x in crop_names ]
     
-        
     # get SIFT Features
-    sift_desc = extractFeatures(crops, getSIFTfeatures)
-    sift_histograms = generateHistograms(sift_desc, sift_kmeans[person])
+    sift_histograms = generateHistograms(allSIFT[person], sift_kmeans)
     sift_test_feature_vec[0].extend(sift_histograms)
     sift_test_feature_vec[1].extend([person] * len(sift_histograms))
     print("Extracted SIFT")
     
     # get SURF Features
-    surf_desc = extractFeatures(crops, getSURFfeatures)
-    surf_histograms = generateHistograms(surf_desc, surf_kmeans[person])
+    surf_histograms = generateHistograms(allSURF[person], surf_kmeans)
     surf_test_feature_vec[0].extend(surf_histograms)
     surf_test_feature_vec[1].extend([person] * len(surf_histograms))
     print("Extracted SURF")
     
     # get ORB Features
-    orb_desc = extractFeatures(crops, getORBfeatures)
-    orb_histograms = generateHistograms(orb_desc, orb_kmeans[person])
+    orb_histograms = generateHistograms(allORB[person], orb_kmeans)
     orb_test_feature_vec[0].extend(orb_histograms)
     orb_test_feature_vec[1].extend([person] * len(orb_histograms))
     print("Extracted ORB")
     
 
     
-#%% Train SVM
+##%% Train SVM
  
 print("Training SVM")
 svmModel = svm.SVC(gamma = 'auto')
--- orc.py
+++ orc.py
@@ -8,59 +8,148 @@ Created on Tue Apr  9 17:34:27 2019
 
 import cv2 
 import numpy as np
+import matplotlib.pyplot as plt
 import pytesseract
-import imutils
+
+#img_path = "sophie_side.jpeg"
+#img_path = "jack_side.jpeg"# --> not so good
+#img_path = "jack.jpeg"#
+#img_path = "sepher.jpg"#
+
+#img_path = "./IMG_3546.jpeg"
+#img_path = "./IMG_3542.jpeg"
+img_path = "./IMG_20190128_201734.jpg"
+#vid_path = "./IMG_20190128_201734.mp4"
 
 
-img_path = "IMG_3542.jpeg"
-#img_path = "./IMG_20190128_201734.jpg"
-vid_path = "./IMG_20190128_201734.mp4"
-east_dect = "./frozen_east_text_detection.pb"
 
+def showImage(img):
+    cv2.namedWindow("hunk", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('hunk', 600,600)
+    cv2.imshow('hunk', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 img =  cv2.imread(img_path)
+print(img.shape)
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#%% Threshold
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-cv2.namedWindow("hunk", cv2.WINDOW_NORMAL)
-cv2.resizeWindow('hunk', 600,600)
-
+retval, graythresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+#showImage(graythresh)
 
 
-#hue ,saturation ,value = cv2.split(hsv)
-#two = np.hstack((value, gray))
-#retval, graythresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-#retval, valuethresh = cv2.threshold(value, 200, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-#thresh =np.hstack((graythresh, valuethresh))
-#th = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 301, 10)
-
-
-#color thresholding
-lower = np.array([200,200,200])
-upper = np.array([255,255,255])
-
-mask = cv2.inRange(img, lower, upper)
-res = cv2.bitwise_and(img,img, mask= mask)
-
-
-#opening to remove noise
+#%% Opening
 kernel = np.ones((100,100),np.uint8)
-opening = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel)
-
-gray = cv2.cvtColor(opening, cv2.COLOR_RGB2GRAY)
-contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-c_img = cv2.drawContours(img, contours, -1, (0,255,0), 3)
+opening = cv2.morphologyEx(graythresh, cv2.MORPH_OPEN, kernel)
 
 
+#showImage(opening)
+#%% MSER
 
-#two = np.hstack((res, opening))
+rois = []
 
-cv2.imshow('hunk',c_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+vis = img.copy()
+mser = cv2.MSER_create(_delta=10, _max_area=16000, _max_variation=0.1)
+regions, _ = mser.detectRegions(opening)
+for p in regions:
+    xmax, ymax = np.amax(p, axis=0)
+    xmin, ymin = np.amin(p, axis=0)
+    xmax, ymax = xmax + 30, ymax + 30
+    xmin, ymin = xmin - 30, ymin - 30
+    rois.append(vis[ymin:ymax, xmin:xmax])
+    #cv2.rectangle(vis, (xmin,ymax), (xmax,ymin), (0, 255, 0), 4)
+
+  
+#showImage(vis)
+print(len(rois), "possible images")
+
+#%% filtering by histogram
+best_roi = None
+best_pct = .88
+for img in rois:
+  plt.imshow(img)
+  plt.show()
+  #showImage(img)
+  gimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  hist,bins = np.histogram(gimg.ravel(),16)
+  
+  #print(hist)
+
+  low = sum(hist[:4])
+  high = sum(hist[12:])
+  al = sum(hist)
+  bimodal_pct = (low+high)/al
+  print(bimodal_pct)
+  
+  plt.hist(img.ravel(),16)
+  plt.show()
+  
+  if bimodal_pct > best_pct:
+      best_roi = img
+      best_pct = bimodal_pct
+
+
+plt.imshow(best_roi)
+plt.show()
+
+#%% deskew
+g_roi = cv2.cvtColor(best_roi, cv2.COLOR_BGR2GRAY)
+gray = cv2.bitwise_not(g_roi)
+
+# threshold the image, setting all foreground pixels to
+# 255 and all background pixels to 0
+thresh = cv2.threshold(gray, 0, 255,
+	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+#cv2_imshow(thresh)
+
+# grab the (x, y) coordinates of all pixel values that
+# are greater than zero, then use these coordinates to
+# compute a rotated bounding box that contains all
+# coordinates
+coords = np.column_stack(np.where(thresh > 0))
+angle = cv2.minAreaRect(coords)[-1]
+print(angle)
+ 
+# the `cv2.minAreaRect` function returns values in the
+# range [-90, 0); as the rectangle rotates clockwise the
+# returned angle trends to 0 -- in this special case we
+# need to add 90 degrees to the angle
+if angle < -45:
+	angle = -(90 + angle)
+ 
+# # otherwise, just take the inverse of the angle to make
+# # it positive
+else:
+	angle = -angle
+  
+print(angle)
+
+
+# rotate the image to deskew it
+image = best_roi.copy()
+(h, w) = image.shape[:2]
+center = (w // 2, h // 2)
+M = cv2.getRotationMatrix2D(center, angle, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h),
+	flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+
+
+ret, thresh1 = cv2.threshold(rotated, 150,255,cv2.THRESH_BINARY)
+plt.imshow(thresh1)
+plt.show()
+
+#%% pytesseract
+
+
+
+config = ("-l eng --oem 1 --psm 7 outputbase digits")
+text = pytesseract.image_to_string(thresh1, config=config) 
+print(text)
+
 
 
 # https://docs.opencv.org/3.4/d7/d1b/group__imgproc__misc.html#ga72b913f352e4a1b1b397736707afcde3
